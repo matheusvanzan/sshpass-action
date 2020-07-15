78cc1bdcfa06b8e774e9420737fa865204deb92e
Matheus Vanzan <vanzan2015@gmail.com>
2020-07-15 10:53:00 -0300

    add run

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,12 @@ echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 HOST=$1
 USERNAME=$2
 PASSWORD=$3
+RUN=$4
 
 echo "HOST $HOST"
 echo "USERNAME $USERNAME"
 echo "PASSWORD $PASSWORD"
+echo "RUN $RUN"
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
