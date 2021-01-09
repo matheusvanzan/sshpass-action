cb2d9f98cf14c49291e315d4e223e822fa4e6cbe
Matheus Vanzan <vanzan2015@gmail.com>
2021-01-09 17:51:17 -0300

    test passing args as env

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,10 +15,13 @@ echo "INPUT_USER: ${INPUT_USER}"
 echo "INPUT_PASS: ${INPUT_PASS}"
 echo "INPUT_RUN: ${INPUT_RUN}"
 
+HOST=${INPUT_HOST}
+USER=${INPUT_USER}
 export SSHPASS=${INPUT_PASS}
 
-CMD="${INPUT_RUN/$'\n'/' && '}"
-sshpass -e ssh -o StrictHostKeyChecking=no ${INPUT_USER}@${INPUT_HOST} "$CMD"
+RUN=${INPUT_RUN}
+CMD="${RUN/$'\n'/' && '}"
+sshpass -e ssh -o StrictHostKeyChecking=no $USER@$HOST "$CMD"
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
