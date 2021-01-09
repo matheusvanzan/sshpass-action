4d8fab882b8552830c2ed0d9dba4b9eb13782cec
Matheus Vanzan <vanzan2015@gmail.com>
2021-01-09 17:58:10 -0300

    test passing args as env

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,23 +5,18 @@ echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
 # Available env
-# INPUT_HOST
-# INPUT_USER
-# INPUT_PASS
-# INPUT_RUN
-
 echo "INPUT_HOST: ${INPUT_HOST}"
-echo "INPUT_USER: ${INPUT_USER}"
-echo "INPUT_PASS: ${INPUT_PASS}"
+echo "INPUT_USERNAME: ${INPUT_USERNAME}"
+echo "INPUT_PASSWORD: ${INPUT_PASSWORD}"
 echo "INPUT_RUN: ${INPUT_RUN}"
 
 HOST=${INPUT_HOST}
-USER=${INPUT_USER}
-export SSHPASS=${INPUT_PASS}
+USERNAME=${INPUT_USERNAME}
+export SSHPASS=${INPUT_PASSWORD}
 
 RUN=${INPUT_RUN}
 CMD="${RUN/$'\n'/' && '}"
-sshpass -e ssh -o StrictHostKeyChecking=no $USER@$HOST "$CMD"
+sshpass -e ssh -o StrictHostKeyChecking=no $USERNAME@$HOST "$CMD"
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
