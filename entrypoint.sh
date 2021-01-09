ac10fd3a43687b9dc2dd1ab8b0cfa4d22c535b74
Matheus Vanzan <vanzan2015@gmail.com>
2021-01-09 19:56:32 -0300

    v2

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
 # Available env
-# echo "INPUT_HOST: ${INPUT_HOST}"
-# echo "INPUT_PORT: ${INPUT_PORT}"
-# echo "INPUT_USER: ${INPUT_USER}"
-# echo "INPUT_PASS: ${INPUT_PASS}"
-# echo "INPUT_KEY: ${INPUT_KEY}"
-# echo "INPUT_RUN: ${INPUT_RUN}"
+echo "INPUT_HOST: ${INPUT_HOST}"
+echo "INPUT_PORT: ${INPUT_PORT}"
+echo "INPUT_USER: ${INPUT_USER}"
+echo "INPUT_PASS: ${INPUT_PASS}"
+echo "INPUT_KEY: ${INPUT_KEY}"
+echo "INPUT_RUN: ${INPUT_RUN}"
 
 CMD="${INPUT_RUN/$'\n'/' && '}"
 
